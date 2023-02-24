5d91106220016c3d66aec26bfe5749edc92497d0
Igor <jaknikig@mail.ru>
2023-02-25 00:42:45 +0600

    init

---
+++ seminar1task04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
