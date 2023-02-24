9773a1c89661d826e7bbe64adccf6baa41ddc164
Igor <jaknikig@mail.ru>
2023-02-25 00:42:31 +0600

    init

---
+++ seminar1task03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int numberMax = 0;
+if (number1>number2)
+{
+    numberMax = number1;
+}
+else
+{
+    numberMax = number2;
+}
+if (numberMax < number3)
+{
+    numberMax = number3;
+}
+Console.WriteLine("Максимум " + numberMax);
