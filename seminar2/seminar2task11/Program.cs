21a17c731fddf94c0c8507cffc63fb6285ab315f
Igor <jaknikig@mail.ru>
2023-03-17 23:44:08 +0600

    init

---
+++ seminar2/seminar2task11/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if(number1 / 100 > 0)
+     {while(number1 > 1000)
+         {
+             number1 = number1 / 10;
+         }
+    Console.WriteLine("Third number " + number1 % 10);
+    }
+else
+    {
+    Console.WriteLine("Third number not exists ");
+    }
+
+
+//325- остаток от деления
+//3256- разделить на 10, остаток от деления
+//32569- разделить на 100, остаток от деления
+//325697- разделить на 1000, остаток от деления
+
+
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// int numberMax = 0;
+// if (number1>number2)
+// {
+//     numberMax = number1;
+// }
+// else
+// {
+//     numberMax = number2;
+// }
+// if (numberMax < number3)
+// {
+//     numberMax = number3;
+// }
+// Console.WriteLine("Максимум " + numberMax);
---
+++ seminar2/seminar2task15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая 
+// принимает на вход цифру, обозначающую день недели, и 
+// проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if(number1 / 100 > 0)
+     {while(number1 > 1000)
+         {
+             number1 = number1 / 10;
+         }
+    Console.WriteLine("Third number " + number1 % 10);
+    }
+else
+    {
+    Console.WriteLine("Third number not exists ");
+    }
